60db8a89799dd85fdac98d70f64182ba43a616fc
iceinstitut <108342553+iceinstitut@users.noreply.github.com>
2025-09-07 12:49:54 +0700

    Update load-head.js

--- load-head.js
+++ load-head.js
@@ -1,5 +1,12 @@
-fetch("/konten/head.html")
-  .then(response => response.text())
-  .then(data => {
-    document.head.insertAdjacentHTML("beforeend", data);
-  });
+function loadComponent(id, url) {
+  fetch("/konten/" + url)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  loadComponent("header", "header.html");
+  loadComponent("footer", "footer.html");
+});
